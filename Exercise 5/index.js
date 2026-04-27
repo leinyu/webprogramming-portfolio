ef1121fc1d0dd5b550db46ee2f98d4d738858643
Nathaniel <josemaritalens@gmail.com>
2026-04-27 08:33:25 +0000

    Added 95% of the audios for exercise 5

--- Exercise 5/index.js
+++ Exercise 5/index.js
@@ -12,18 +12,18 @@ ttsButton.addEventListener('click', () => {
 });
 
 const allSamples = [
-    { id: 10, title: "Aghhhhh", file: "10.mp3", time: "11.0" },
+    { id: 10, title: "Ah-ha", file: "10.mp3", time: "1.4" },
     { id: 11, title: "Dan", file: "11.mp3", time: "0.5" },
-    { id: 12, title: "Back of the net", file: "12.mp3", time: "8.00" },
-    { id: 13, title: "Bang out of order", file: "13.mp3", time: "2.00" },
-    { id: 14, title: "Jurassic Park", file: "14.mp3", time: "4.00" },
-    { id: 15, title: "Smell my cheese", file: "15.mp3", time: "1.91" },
-    { id: 16, title: "Goal", file: "16.mp3", time: "1.49" },
+    { id: 12, title: "Back of the net", file: "12.mp3", time: "1.02" },
+    { id: 13, title: "Bang out of order", file: "13.mp3", time: "1.38" },
+    { id: 14, title: "Email of the Evening", file: "14.mp3", time: "1.49" },
+    { id: 15, title: "Scotch Egg", file: "15.mp3", time: "1.91" },
+    { id: 16, title: "Im Confused", file: "16.mp3", time: "1.49" },
     { id: 17, title: "Kiss my face", file: "17.mp3", time: "1.07" },
     { id: 18, title: "Hello Partridge", file: "18.mp3", time: "2.25" },
     // Page 2 Items
-    { id: 19, title: "Monkey Tennis", file: "19.mp3", time: "1.20" },
-    { id: 20, title: "Aha!!", file: "20.mp3", time: "0.85" }
+    { id: 19, title: "Wii Tennis", file: "19.mp3", time: "5.2" },
+    { id: 20, title: "Ah-ah", file: "20.mp3", time: "4.8" }
 ];
 
 let currentPage = 0;
